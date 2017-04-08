c6cb41278b3386b8eb318593a3272053d764554c
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-07 21:39:58 -0600

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -26,8 +26,6 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
@@ -38,8 +36,6 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
-# The page updates again, and now shows both items on her list
 
 # Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some
